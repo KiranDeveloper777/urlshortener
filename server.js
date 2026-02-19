3f43342d01dc5940cc011f713e01048fb0572d07
KiranDeveloper777 <kirant200513@gmail.com>
2026-02-19 12:19:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.get("/api/shorturl/:short_url", (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
